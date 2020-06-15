f97121365927184f0db5dc7ac3505672a6e4ca79
hotchemi <shintaro.katafuchi@smartnews.com>
2020-06-16 02:11:12 +0900

    fix: screen orientation issue

--- ktx/src/main/java/permissions/dispatcher/ktx/PermissionsRequestFragment.kt
+++ ktx/src/main/java/permissions/dispatcher/ktx/PermissionsRequestFragment.kt
@@ -3,9 +3,11 @@ package permissions.dispatcher.ktx
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import android.content.res.Configuration.ORIENTATION_PORTRAIT
 import android.net.Uri
 import android.os.Build
+import android.os.Bundle
 import android.provider.Settings
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
@@ -18,26 +20,44 @@ internal class PermissionsRequestFragment : Fragment() {
     private var requiresPermission: Func? = null
     private var onNeverAskAgain: Func? = null
     private var onPermissionDenied: Func? = null
+    private var requestedOrientation: Int? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         retainInstance = true
-        activity?.requestedOrientation =
-            if (context.resources.configuration.orientation == ORIENTATION_PORTRAIT)
-                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-            else
-                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        requestedOrientation = activity?.requestedOrientation
+        activity?.requestedOrientation = when (resources.configuration.orientation) {
+            ORIENTATION_PORTRAIT -> ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            ORIENTATION_LANDSCAPE -> ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            else -> ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        requestedOrientation?.let {
+            outState.putInt(KEY_ORIENTATION, it)
+        }
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        requestedOrientation = savedInstanceState?.getInt(KEY_ORIENTATION)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        requestedOrientation?.let { activity?.requestedOrientation = it }
         requiresPermission = null
         onNeverAskAgain = null
         onPermissionDenied = null
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == this.requestCode) {
             if (verifyPermissions(*grantResults)) {
@@ -96,6 +116,7 @@ internal class PermissionsRequestFragment : Fragment() {
         fragmentManager?.beginTransaction()?.remove(this)?.commitNowAllowingStateLoss()
 
     companion object {
+        private const val KEY_ORIENTATION = "key:orientation"
         val tag = PermissionsRequestFragment::class.java.canonicalName
         fun newInstance() = PermissionsRequestFragment()
     }
